0005c5b65824e4f843653bab0cf46c1cea652f8a
jingx <xin.jing1@thoughtworks.com>
2020-06-10 00:46:34 +0800

    [Xin] Finish the Practice1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -9,8 +10,47 @@ import java.time.LocalDate;
  * @create 2020-05-15_16:33
  */
 public class Practice1 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2020, 5, 1);
+        boolean isAhead = isTheYearAheadOfNow(date);
+        if (isAhead) {
+            System.out.println("The year you choose is ahead the real year.");
+        } else {
+            long result = getDaysBetweenNextLaborDay(date);
+            outputTheResult(result);
+        }
+    }
 
-  public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
-  }
+    public static long getDaysBetweenNextLaborDay(LocalDate date) {
+        long daysBetween;
+        int year = date.getYear();
+        LocalDate thisYearLaborDay = LocalDate.of(year, 5, 1);
+
+        boolean isBefore = date.isBefore(thisYearLaborDay);
+
+        if (isBefore) {
+            daysBetween = thisYearLaborDay.toEpochDay() - date.toEpochDay();
+        } else {
+            LocalDate nextYearLaborDay = thisYearLaborDay.plusYears(1);
+            daysBetween = nextYearLaborDay.toEpochDay() - date.toEpochDay();
+        }
+        return daysBetween;
+    }
+
+    private static boolean isTheYearAheadOfNow(LocalDate date) {
+        int year = date.getYear();
+        int thisYear = LocalDate.now().getYear();
+        if (year < thisYear) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void outputTheResult(long result) {
+        if (result != 0) {
+            System.out.println("There are still " + result + " to the next Labor day. Back to work! NOW!!!");
+        } else {
+            System.out.println("Congratulations, Today is the Labor Day!");
+        }
+    }
 }
