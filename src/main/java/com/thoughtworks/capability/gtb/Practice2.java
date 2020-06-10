65a0b033efefe71534ec2bedf3f95293dc92f506
jingx <xin.jing1@thoughtworks.com>
2020-06-10 11:11:16 +0800

    [Xin] Finish the Practice2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
@@ -9,8 +10,27 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2020, 5, 1);
+        LocalDate nextWorkDay = getNextWorkDate(date);
+        System.out.println(nextWorkDay);
+    }
 
-  public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
-  }
+    public static LocalDate getNextWorkDate(LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        int dayToAdd = calculateDaysToAdd(dayOfWeek);
+        LocalDate nextWorkDay = date.plusDays(dayToAdd);
+        return nextWorkDay;
+    }
+
+    private static Integer calculateDaysToAdd(DayOfWeek dayOfWeek) {
+        int dayToAdd = 1;
+        if (dayOfWeek == DayOfWeek.FRIDAY) {
+            dayToAdd = 3;
+        }
+        if (dayOfWeek == DayOfWeek.SATURDAY) {
+            dayToAdd = 2;
+        }
+        return dayToAdd;
+    }
 }
